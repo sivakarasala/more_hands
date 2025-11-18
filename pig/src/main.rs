4897660e23502a2f29759978611cb6d5760c725b
Siva Karasala <shivataraya@gmail.com>
2025-11-18 23:31:36 +0530

    feat: add dice game and integrate rand lib

---
+++ pig/src/main.rs
@@ -0,0 +1,186 @@
+use bevy::prelude::*;
+use bevy_egui::{EguiContexts, EguiPlugin, egui};
+use my_library::RandomNumberGenerator;
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug, Hash, Default, States)]
+enum GamePhase {
+    #[default]
+    Player,
+    Cpu,
+}
+
+#[derive(Resource)]
+struct GameAssets {
+    image: Handle<Image>,
+    layout: Handle<TextureAtlasLayout>,
+}
+
+#[derive(Clone, Copy, Resource)]
+struct Scores {
+    player: usize,
+    cpu: usize,
+}
+
+#[derive(Component)]
+struct HandDie;
+
+#[derive(Resource)]
+struct Random(RandomNumberGenerator);
+
+#[derive(Resource)]
+struct HandTimer(Timer);
+
+fn setup(
+    asset_server: Res<AssetServer>,
+    mut commands: Commands,
+    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
+) {
+    commands.spawn(Camera2d::default());
+
+    let texture = asset_server.load("dice.png");
+    let layout = TextureAtlasLayout::from_grid(UVec2::splat(52), 6, 1, None, None);
+    let texture_atlas_layout = texture_atlas_layouts.add(layout);
+
+    commands.insert_resource(GameAssets {
+        image: texture,
+        layout: texture_atlas_layout,
+    });
+    commands.insert_resource(Scores { cpu: 0, player: 0 });
+    commands.insert_resource(Random(RandomNumberGenerator::new()));
+    commands.insert_resource(HandTimer(Timer::from_seconds(0.5, TimerMode::Repeating)));
+}
+
+fn display_score(scores: Res<Scores>, mut egui_context: EguiContexts) {
+    egui::Window::new("Total Scores").show(egui_context.ctx_mut(), |ui| {
+        ui.label(&format!("Player: {}", scores.player));
+        ui.label(&format!("CPU: {}", scores.cpu));
+    });
+}
+
+fn spawn_die(
+    hand_query: &Query<(Entity, &Sprite), With<HandDie>>,
+    commands: &mut Commands,
+    assets: &GameAssets,
+    new_roll: usize,
+    color: Color,
+) {
+    let rolled_die = hand_query.iter().count() as f32 * 52.0;
+    let mut sprite = Sprite::from_atlas_image(
+        assets.image.clone(),
+        TextureAtlas {
+            layout: assets.layout.clone(),
+            index: new_roll - 1,
+        },
+    );
+    sprite.color = color;
+    commands.spawn((
+        sprite,
+        Transform::from_xyz(rolled_die - 400.0, 60.0, 1.0),
+        HandDie,
+    ));
+}
+
+fn clear_die(hand_query: &Query<(Entity, &Sprite), With<HandDie>>, commands: &mut Commands) {
+    hand_query
+        .iter()
+        .for_each(|(entity, _)| commands.entity(entity).despawn());
+}
+
+fn player(
+    hand_query: Query<(Entity, &Sprite), With<HandDie>>,
+    mut commands: Commands,
+    mut rng: ResMut<Random>,
+    assets: Res<GameAssets>,
+    mut scores: ResMut<Scores>,
+    mut state: ResMut<NextState<GamePhase>>,
+    mut egui_context: EguiContexts,
+) {
+    egui::Window::new("Play Options").show(egui_context.ctx_mut(), |ui| {
+        let hand_score: usize = hand_query
+            .iter()
+            .map(|(_, ts)| ts.texture_atlas.as_ref().unwrap().index + 1)
+            .sum();
+        ui.label(&format!("Score for this hand: {hand_score}"));
+
+        if ui.button("Roll Dice").clicked() {
+            let new_roll = rng.0.range(1..7);
+            if new_roll == 1 {
+                // End turn!
+                clear_die(&hand_query, &mut commands);
+                state.set(GamePhase::Cpu);
+            } else {
+                spawn_die(
+                    &hand_query,
+                    &mut commands,
+                    &assets,
+                    new_roll as usize,
+                    Color::WHITE,
+                );
+            }
+        }
+        if ui.button("Pass - Keep Hand Score").clicked() {
+            let hand_total: usize = hand_query
+                .iter()
+                .map(|(_, ts)| ts.texture_atlas.as_ref().unwrap().index + 1)
+                .sum();
+            scores.player += hand_total;
+            clear_die(&hand_query, &mut commands);
+            state.set(GamePhase::Cpu);
+        }
+    });
+}
+
+#[allow(clippy::too_many_arguments)]
+fn cpu(
+    hand_query: Query<(Entity, &Sprite), With<HandDie>>,
+    mut commands: Commands,
+    mut rng: ResMut<Random>,
+    assets: Res<GameAssets>,
+    mut scores: ResMut<Scores>,
+    mut state: ResMut<NextState<GamePhase>>,
+    mut timer: ResMut<HandTimer>,
+    time: Res<Time>,
+) {
+    timer.0.tick(time.delta());
+    if timer.0.just_finished() {
+        let hand_total: usize = hand_query
+            .iter()
+            .map(|(_, ts)| ts.texture_atlas.as_ref().unwrap().index + 1)
+            .sum();
+        if hand_total < 20 && scores.cpu + hand_total < 100 {
+            let new_roll = rng.0.range(1..7);
+            if new_roll == 1 {
+                clear_die(&hand_query, &mut commands);
+                state.set(GamePhase::Player);
+            } else {
+                spawn_die(
+                    &hand_query,
+                    &mut commands,
+                    &assets,
+                    new_roll as usize,
+                    Color::Srgba(Srgba::new(0.0, 0.0, 1.0, 1.0)),
+                );
+            }
+        } else {
+            scores.cpu += hand_total;
+            state.set(GamePhase::Player);
+            hand_query
+                .iter()
+                .for_each(|(entity, _)| commands.entity(entity).despawn());
+        }
+    }
+}
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(EguiPlugin {
+            enable_multipass_for_primary_context: false,
+        })
+        .add_systems(Startup, setup)
+        .init_state::<GamePhase>()
+        .add_systems(Update, display_score)
+        .add_systems(Update, player.run_if(in_state(GamePhase::Player)))
+        .add_systems(Update, cpu.run_if(in_state(GamePhase::Cpu)))
+        .run();
+}
