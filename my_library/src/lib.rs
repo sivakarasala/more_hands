957507e41fd22e72c91ab454946726e24c3c1ca5
Siva Karasala <shivataraya@gmail.com>
2025-11-22 19:20:54 +0530

    feat: add docs to lib

--- my_library/src/lib.rs
+++ my_library/src/lib.rs
@@ -1,4 +1,28 @@
-pub use rand;
+#![warn(missing_docs)]
+
+//! `my_library` provides a suite of helpers to create games with Bevy.
+//! 
+//! ## What's Included?
+//! 
+//! `my_library` includes:
+//! 
+//! * Random number generation facilities.
+//! 
+//! ## Feature Flags
+//! 
+//! The following feature flags are supported: `xorshift`, `pcg`, `locking`.
+//! 
+//! ### Random Number Generation
+//! 
+//! * The `locking` feature enables interior mutability inside
+//!   [`RandomNumberGenerator`],
+//!   allowing it to be used as a resource (`Res<RandomNumberGenerator>`)
+//!   rather than requiring mutability (`ResMut<RandomNumberGenerator>`)
+//! * You can control which random number generation algorithm is used by
+//!   specifying *one* of:
+//!     * `xorshift` to use the XorShift algorithm.
+//!     * `pcg` to use the PCG algorithm.
+
 #[cfg(not(feature = "locking"))]
 mod random;
 #[cfg(not(feature = "locking"))]
@@ -8,3 +32,7 @@ pub use random::*;
 mod random_locking;
 #[cfg(feature = "locking")]
 pub use random_locking::*;
+
+/// [`RandomNumberGenerator`] wraps the `rand` crate. The `rand` crate
+/// is re-exported for your convenience.
+pub use rand;
